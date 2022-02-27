a2caab5beb5f4af6437800dafb07a53dd4ab233d
BiplopDey <52823579+BiplopDey@users.noreply.github.com>
2022-02-27 13:12:29 +0100

    Remove this.

--- src/services/pokemonCardListService.js
+++ src/services/pokemonCardListService.js
@@ -1,5 +1,4 @@
 export function PokemonListService(repository) {
-  this.repository = repository;
   this.getPokemonsIdByPage = async function (page) {
     return await repository.getPokemonsIdByPage(page);
   };
